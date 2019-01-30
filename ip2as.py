11debbe21c4033693e19fe5387713b5d7fb40fda
Kishan Patel <33336217+kishanpatel97@users.noreply.github.com>
2019-01-29 16:42:13 -0800

    Add files via upload

---
+++ ip2as.py
@@ -0,0 +1,16 @@
+def findAS(line, dbfile):
+    
+
+
+
+
+
+
+def main(argv):
+    inputfilename = argv[2]
+    file = open(inputfilename, "r")
+        for line in file:
+            findAS(line, argv[1])
+
+if __name__ == "__main__":
+    main(sys.argv[2])
